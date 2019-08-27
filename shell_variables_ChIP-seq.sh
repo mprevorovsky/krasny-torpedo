ce7f7a5f1b608aca0cfa3b223701b965fc9b8233
mprevorovsky <mprevorovsky@volny.cz>
2019-08-27 14:48:12 +0200

    setting up IP bam normalization

--- shell_variables_ChIP-seq.sh
+++ shell_variables_ChIP-seq.sh
@@ -16,4 +16,8 @@ genome='./genome/Bacillus_subtilis_subsp._subtilis_str._168.fasta'
 bam_dir="./BAM_ChIP-seq/"
 # minimum MAPQ threshold for inclusion in the final BAM file
 Q=10
+# output directory for genome coverage data
+coverage_dir="./coverage/"
+# file containing the list of all BAM files to be processed
+bam_file_list="BAM_files_ChIP-seq"
 

e7adc7be3272d502cac9f0ee3b1bb46a6c6cc17a
mprevorovsky <mprevorovsky@volny.cz>
2019-08-30 14:50:08 +0200

    finished ChIP-seq, started RNA-seq analysis

--- shell_variables_RNA-seq.sh
+++ shell_variables_RNA-seq.sh
@@ -3,28 +3,26 @@
 # CPU threads
 CPU=4
 # output directory for QC
-fastQC_dir="./fastQC_ChIP-seq/"
+fastQC_dir="./fastQC_RNA-seq/"
 # FASTQ file directory
-fastq_dir="./FASTQ_ChIP-seq/"
+fastq_dir="./FASTQ_RNA-seq/"
 # file containing the list of all FASTQ files to be processed
-fastq_file_list="FASTQ_files_ChIP-seq"
+fastq_file_list="FASTQ_files_RNA-seq"
 # FASTQ file extension
 fastq_file_ext=".txt.gz$"
 # file containing reference genome sequence
 genome='./genome/Bacillus_subtilis_subsp._subtilis_str._168.fasta'
 # BAM file directory
-bam_dir="./BAM_ChIP-seq/"
+bam_dir="./BAM_RNA-seq/"
 # minimum MAPQ threshold for inclusion in the final BAM file
 Q=10
 # output directory for genome coverage data
 coverage_dir="./coverage_ChIP-seq/"
 # file containing the list of all BAM files to be processed
-bam_file_list="BAM_files_ChIP-seq"
-# filename extension for input-normalized genome coverage files
-normalized_suffix=".input-normalized"
+bam_file_list="BAM_files_RNA-seq"
 # string contained in KO sample file names
 ko="KO"
 # string contained in WT sample file names
-wt="wt"
+wt="WT"
 # images directry
 image_dir="./images/"

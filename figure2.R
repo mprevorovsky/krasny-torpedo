2bc7204f53a84a9510ff8c4fcff1a67d1194b26f
mprevorovsky <mprevorovsky@volny.cz>
2019-09-06 13:14:26 +0200

    relabelled figures

--- figure2.R
+++ figure2.R
@@ -1,47 +1,86 @@
-library(RColorBrewer)
-chip <- read.delim('./coverage_ChIP-seq/ChIP-seq_gene_coverage.txt', 
-                   sep = '\t', header = TRUE, stringsAsFactors = FALSE)
-rna <- read.delim('./coverage_RNA-seq/RNA-seq_gene_coverage.txt', 
-                   sep = '\t', header = TRUE, stringsAsFactors = FALSE)
-which(chip$Name != rna$Name)
+# https://stat.ethz.ch/pipermail/bioconductor/2008-October/024669.html
+getAttributeField <- function (x, field, attrsep = ";") {
+  s = strsplit(x, split = attrsep, fixed = TRUE)
+  sapply(s, function(atts) {
+    a = strsplit(atts, split = "=", fixed = TRUE)
+    m = match(field, sapply(a, "[", 1))
+    if (!is.na(m)) {
+      rv <- a[[m]][2]
+    }
+    else {
+      rv <- as.character(NA)
+    }
+    return(rv)
+  })
+}
 
-pdf('./images/Figure2.pdf')
-par(mar = c(5, 5, 5, 5), lwd = 2, cex = 1.5)
-options(scipen = -1)
-max.chip <- max(chip[, c('cov_wt', 'cov_ko')], na.rm = TRUE)
-min.chip <- 10
-max.rna <- max(rna[, c('cov_wt', 'cov_ko')], na.rm = TRUE)
-min.rna <- 0.1
+gffRead <- function(gffFile, nrows = -1) {
+  cat("Reading ", gffFile, ": ", sep="")
+  gff = read.table(gffFile, sep="\t", as.is=TRUE, quote="",
+                   header=FALSE, comment.char="#", nrows = nrows,
+                   colClasses=c("character", "character", "character", "integer",  
+                                "integer",
+                                "character", "character", "character", "character"))
+  colnames(gff) = c("seqname", "source", "feature", "start", "end",
+                    "score", "strand", "frame", "attributes")
+  cat("found", nrow(gff), "rows with classes:",
+      paste(sapply(gff, class), collapse=", "), "\n")
+  stopifnot(!any(is.na(gff$start)), !any(is.na(gff$end)))
+  return(gff)
+}
 
-rbPal <- colorRampPalette(c('yellow', 'red'))
+genelist <- c('ldh', 'ydbL', 'ymcB', 'bsrB', 'veg', 'rpmI', 'hag', 'purR', 'pyrR')
 
-plot(chip$cov_ko, rna$cov_ko,
-     main = '',
-     xlab = '',
-     ylab = '',
-     log = 'xy', xlim = c(min.chip, max.chip), ylim = c(min.rna, max.rna),
-     type = 'n')
-abline(v = c(10, 100, 1000, 10000, 100000), h = c(0.1, 10, 1000, 100000), col = 'gray')
-par(new = TRUE)
-plot(chip$cov_ko, rna$cov_ko,
-     main = 'rnjA KO\nmean of 3 replicates\n(coloured by expression in WT)',
-     xlab = 'RNAP occupancy at genes [norm. cov.]',
-     ylab = 'gene RNA abundance [norm. cov.]',
-     log = 'xy', xlim = c(min.chip, max.chip), ylim = c(min.rna, max.rna), pch = 20,
-     col = rbPal(100)[as.numeric(cut(log2(rna$cov_wt + 0.01), breaks = 100))])
+gff_file <- './genome/GCF_000009045.1_ASM904v1_genomic_reverse_SYS.gff'
+gff <- gffRead(gff_file)
+gff <- gff[gff$feature == 'gene', ]
+gff$Name <- getAttributeField(gff$attributes, 'Name')
+gff$locus_tag <- getAttributeField(gff$attributes, 'locus_tag')
+gff[which(gff$locus_tag == 'BSU_misc_RNA_32'), 'locus_tag'] <- 'bsrB'
 
-plot(chip$cov_wt, rna$cov_wt,
-     main = '',
-     xlab = '',
-     ylab = '',
-     log = 'xy', xlim = c(min.chip, max.chip), ylim = c(min.rna, max.rna),
-     type = 'n')
-abline(v = c(10, 100, 1000, 10000, 100000), h = c(0.1, 10, 1000, 100000), col = 'gray')
-par(new = TRUE)
-plot(chip$cov_wt, rna$cov_wt,
-     main = 'WT\nmean of 3 replicates\n(coloured by expression in WT)',
-     xlab = 'RNAP occupancy at genes [norm. cov.]',
-     ylab = 'gene RNA abundance [norm. cov.]',
-     log = 'xy', xlim = c(min.chip, max.chip), ylim = c(min.rna, max.rna), pch = 20,
-     col = rbPal(100)[as.numeric(cut(log2(rna$cov_wt + 0.01), breaks = 100))])
-dev.off()
+ko <- read.csv('./coverage_ChIP-seq/KO.input-normalized.mean.bedgraph', head = FALSE, stringsAsFactors = FALSE, skip = 1, sep = '\t')
+wt <- read.csv('./coverage_ChIP-seq/wt.input-normalized.mean.bedgraph', head = FALSE, stringsAsFactors = FALSE, skip = 1, sep = '\t')
+data <- rbind(wt, ko)
+
+for (i in genelist) {
+  start <- gff[which(gff$locus_tag == i) - 1, 'start']
+  end <- gff[which(gff$locus_tag == i) + 1, 'end']
+  chr <- gff[which(gff$locus_tag == i), 'seqname']
+  ymax <- max(data[data[, 2] >= start & data[, 2] <= end, 4], na.rm = TRUE)
+  ymin <- (-1 * ymax) / 4
+
+  pdf(file = paste0('./images/', start, '-', end, '_', i, '.pdf'), title = paste0(i, '.pdf'))
+  par(mfrow = c(2, 1), mar = c(3, 4, 4, 2))
+  
+  plot(wt[which(wt[, 1] == chr & wt[, 2] >= start & wt[, 2] <= end), c(2, 4)],
+       type = 'h', col = 'blue', ylim = c(ymin, ymax), 
+       main = 'WT', xlab = '', ylab = 'normalized coverage', cex.axis = 1.2, cex.lab = 1.2)
+  features = gff[gff$seqname == chr & (gff$start <= end & gff$end >= start), ]
+  for (i in 1:nrow(features)){
+    if (features[i, 'strand'] == '-'){
+      arrows(features[i, 'start'], ymin, features[i, 'end'], ymin, lwd = 4, length = 0.1)
+    }
+    else{
+      arrows(features[i, 'end'], ymin, features[i, 'start'], ymin, lwd = 4, length = 0.03)
+    }
+  }
+  text(labels = features$locus_tag, x = features$start + (features$end - features$start) / 2, 
+       y = ymin + ymax / 10, cex = 1.5)
+  
+  plot(ko[which(ko[, 1] == chr & ko[, 2] >= start & ko[, 2] <= end), c(2, 4)],
+       type = 'h', col = 'red', ylim = c(ymin, ymax), 
+       main = 'rnjA', xlab = '', ylab = 'normalized coverage', cex.axis = 1.2, cex.lab = 1.2)
+  features = gff[gff$seqname == chr & (gff$start <= end & gff$end >= start), ]
+  for (i in 1:nrow(features)){
+    if (features[i, 'strand'] == '-'){
+      arrows(features[i, 'start'], ymin, features[i, 'end'], ymin, lwd = 4, length = 0.1)
+    }
+    else{
+      arrows(features[i, 'end'], ymin, features[i, 'start'], ymin, lwd = 4, length = 0.03)
+    }
+  }
+  text(labels = features$locus_tag, x = features$start + (features$end - features$start) / 2, 
+       y = ymin + ymax / 10, cex = 1.5)
+  
+  dev.off()  
+}
